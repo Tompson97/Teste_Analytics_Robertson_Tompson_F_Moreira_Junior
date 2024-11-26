5bfdf0b504c2cb6a0045ccdf5665c2922604e752
Tompson Moreira <robertsontompson.mjr@gmail.com>
2024-11-25 20:05:46 -0400

    att script de limpeza de dados

---
+++ teste_analytics/Limpeza e Análise de Dados.py
@@ -0,0 +1,74 @@
+# Este script atenderá a primeira etapa da parte 1 das tarefas propostas:
+# Limpeza e Análise de Dados de Vendas.
+
+# Instalando dependências necessárias:
+# pip install pandas
+
+# Importando a biblioteca Pandas para analisar dados de origem .xlsx (Excel).
+import pandas as pd
+# Usaremos o "pd" para referenciar essa biblioteca.
+
+
+# Carregando dados da simulação para o ambiente de desenvolvimento.
+dados = pd.read_excel(
+    "AgricultureData.xlsx"
+)
+
+# Transformando o conjunto de dados em um Dataframe
+df = pd.DataFrame(dados)
+
+# Exibindo as colunas carregadas.
+print("Colunas carregadas")
+print(df.columns)
+print("\n") # Pulando uma linha para exibir as próximas strings
+
+# Selecionando apenas as colunas que serão necessárias para essa análise.
+df = df[
+    ["order_id","sale_date","product_name","category","units_sold_kg", "price_per_kg"]
+]
+
+# Alterando o nome das colunas para melhor entendimento.
+df = df.rename(
+    columns={
+        "order_id": "id", "sale_date": "Data", "product_name": "Produto",
+        "category": "Categoria", "units_sold_kg": "Quantidade", "price_per_kg": "Preço"
+    }
+)
+
+# Conferindo se existem valores nulos no dataframe
+print("Quantidade de nulos por coluna:")
+print(df.isnull().sum())
+print("\n") # Pulando uma linha para exibir as próximas strings
+# Para este conjunto de dados não foram encontrados nulos.
+# Caso fosse encontrado usariámos a função dropna, /
+# porém dependendo da coluna uma análise seria necessária antes de executar a função
+
+# A chave primária do nosso conjunto de dados é a coluna "id", portanto todos os valores devem ser exclusivos.
+# Checando se existem duplicatas na coluna "id".
+print("Quantidade de duplicatas na coluna 'id':")
+print(df["id"].duplicated().sum())
+print("\n") # Pulando uma linha para exibir as próximas strings
+
+# Checando os tipos de dados de cada coluna.
+print("Tipos de dados de cada coluna:")
+print(df.dtypes)
+print("\n") # Pulando uma linha para exibir as próximas strings
+
+# Exibindo 10 amostras do dataframe para confirmar se o tipo de dado faz sentido.
+print("Amostra com 10 linhas aleatórias do dataframe:")
+print(df.sample(10))
+print("\n") # Pulando uma linha para exibir as próximas strings
+
+# A coluna id é um fator identificador então precisa ser convertida para string.
+# As demais colunas não precisam de ajustes quanto ao tipo de dados.
+
+
+# Convertendo id do tipo inteiro para string.
+df["id"] = df["id"].astype(str)
+
+# A análise proposta pelo cliente diz respeito ao período de 01/01/2023 até 31/12/2023.
+# Portanto, vamos filtrar nosso dataframe para remover linhas desnecessárias fora desse intervalo.
+df = df[(df['Data'] >= '2023-01-01') & (df['Data'] <= '2023-12-31')]
+
+# Salvando os dados tratando em um arquivo .csv
+df.to_csv("data_clean.csv", index=False, encoding='utf-8')
---
+++ teste_analytics/README.md
@@ -0,0 +1,66 @@
+
+# DASHBOARD VAREJO
+## INTRODUÇÃO
+Este projeto foi desenvolvido para criar um dashboard de vendas utilizando as ferramentas MySQL E Power BI. Os dados são fictícios. \
+Inciaremos com a criação da base de dados no MySQL Workbench, tratamento dos dados e quando a estrutura está normalizada vamos conectar no Power BI para desenvolver os relatórios.
+
+## CRIAÇÃO DA BASE DE DADOS 
+Utilizaremos a linguagem MySQL para criar toda a estrutra dos dados, então vamos começar.
+Estou criando um database chamados "vendas" utilizando os padrões de caracteres brasileiros "utf8" para ele aceitar palavras com acentos.\
+Depois vou criar 7 tabelas: Clientes, Produtos, Vendedor, Filial, Pedido e Devoluções.
+```
+create database vendas 
+default character set utf8
+default collate utf8_general_ci;
+```
+Após criar o database vamos selecioná-lo.
+```
+use vendas; 
+```
+Com a base selecionada podemos seguir com a criação da primeira tabela chamada "clientes".
+Usaremos o CPF como chave primária, pois é um dado único dentre todos os registros.
+```
+create table clientes (
+cpf varchar(11) not null,
+nome varchar(20) not null,
+dt_nasc date,
+tel varchar(11),
+primary key(cpf)
+) default charset = utf8;
+```
+Agora vamos inserir alguns dados fictícios na tabela usando esse padrã e depois vamos executar um Select para conferir.
+```
+insert into clientes values
+('12345678901', 'João Silva', '1990-05-15', '11987654321'),
+
+('98765432109', 'Maria Santos', '1985-08-20', '11955551234'),
+
+select * from clientes;
+
+```
+Vamos criar uma coluna para calcular a idade com base na data de nascimento. Como a idade é um número pequeno que não ultrapassará 256 vamos atribuir o tipo tinyint que é semelhante ao int, mas para número menores.
+Você pode utilziar o int, mas o tinyint irá reduzir o espaço ocupado na memória.\
+A função DATEDIFF irá retornar a diferença entre duas datas que na instrução foram CURRENT_DATA data atual (ano de 2024) para dt_nasc que é a data de nascimento, o resultado gerado é em número de dias e façoa divisão por 365 para chegar no número de anos e portanto a idade.
+
+```
+alter table clientes
+add column idade tinyint;
+
+UPDATE clientes
+SET idade = 
+DATEDIFF(CURRENT_DATE, dt_nasc) / 365;
+```
+Com isso finalizamos a criação da tabela clientes.
+Vamos executar um select para conferir o resultado.
+
+```
+select * from clientes
+limit 5;
+```
+| cpf | nome | dt_nasc | tel | idade |
+|--|--|--|--|--|
+| 23456789012  |Carlos Souza |1988-04-05 |11944445555 |36 |
+| 23456789013 | Camila Santos |1994-04-02 | 11966668888 | 30|
+| 23456789014 | Thiago Rodrigues |1997-05-17 | 11966667777 |27|
+| 23456789015 | Vanessa Almeida |1994-01-05 | 11977776666 |31|
+| 12345678901 | João Silva | 1990-05-15 | 11987654321 | 34 |
